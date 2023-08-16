930f0287e5c90c810cf412b9b8839963fd9685b7
Thiago Pratti de Mendonça <135062914+Prattiz@users.noreply.github.com>
2023-08-16 13:17:59 -0300

    Add files via upload

---
+++ SPA/JS/index.js
@@ -0,0 +1,11 @@
+
+const buttonToU = document.querySelector("#button") 
+
+buttonToU.addEventListener("click", (event) =>{
+    event.preventDefault()
+
+    window.location.href = "./universe.html"
+
+})
+
+
